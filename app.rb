ed9f4572488ffc7705c104209ae35590d7fa921d
Marcus Orochena <marcus.orochena@gmail.com>
2014-11-24 12:10:33 -0500

    init commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'shoes'
+
+array = [1,2,3]
+
+
+lines = []
+
+Shoes.app do
+  background white
+  stack(margin: 8) do
+    array.each do |a|
+      a = flow do 
+        edit_line 
+        edit_line
+        button "save"
+        del = button "delete"
+        del.click() do 
+          a.clear
+        end
+      end
+    end
+  end
+end
